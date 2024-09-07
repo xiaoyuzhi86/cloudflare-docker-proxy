09fe332b01598f386529a69ba21c78ad63963802
xiaoyuzhi86 <142903376+xiaoyuzhi86@users.noreply.github.com>
2024-09-07 09:14:05 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,17 +7,17 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.https://xiaoyuzhi.cn/": dockerHub,
+  "quay.xiaoyuzhi.cn": "https://quay.io",
+  "gcr.xiaoyuzhi.cn": "https://gcr.io",
+  "k8s-gcr.xiaoyuzhi.cn": "https://k8s.gcr.io",
+  "k8s.xiaoyuzhi.cn": "https://registry.k8s.io",
+  "ghcr.xiaoyuzhi.cn": "https://ghcr.io",
+  "cloudsmith.xiaoyuzhi.cn": "https://docker.cloudsmith.io",
+  "ecr.xiaoyuzhi.cn": "https://public.ecr.aws",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.xiaoyuzhi.cn": dockerHub,
 };
 
 function routeByHosts(host) {
